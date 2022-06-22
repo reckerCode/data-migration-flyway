f11006ef67fc40a9a6e38ec78aa79363f55f21a3
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-06-22 15:29:52 -0400

    initial commit

---
+++ src/main/java/re/data/migration/flyway/FlywayMigration/FlywayMigrationApplication.java
@@ -0,0 +1,13 @@
+package re.data.migration.flyway.FlywayMigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayMigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/re/data/migration/flyway/FlywayMigration/FlywayMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package re.data.migration.flyway.FlywayMigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
