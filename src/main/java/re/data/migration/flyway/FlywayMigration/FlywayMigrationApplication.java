b60e856d653851fd0b2d712e560b6aecfff0d252
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-06-27 10:02:36 -0400

    flyway example

--- src/main/java/re/data/migration/flyway/FlywayMigration/FlywayMigrationApplication.java
+++ src/main/java/re/data/migration/flyway/FlywayMigration/FlywayMigrationApplication.java
@@ -1,13 +1,35 @@
 package re.data.migration.flyway.FlywayMigration;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class FlywayMigrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlywayMigrationApplication.class, args);
+
+		try {
+			XmlMapper xmlMapper = new XmlMapper();
+			Person pojo = new Person();
+			pojo.setFirstName("Raja");
+			pojo.setLastName("Ramesh");
+			pojo.setAddress("Hyderabad");
+			String xml = xmlMapper.writeValueAsString(pojo);
+			System.out.println(xml);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		Map map = new HashMap();
+		map.getOrDefault(true, Collections.emptySet());
 	}
 
 }
---
+++ src/main/java/re/data/migration/flyway/FlywayMigration/Person.java
@@ -0,0 +1,25 @@
+package re.data.migration.flyway.FlywayMigration;
+
+class Person {
+   private String firstName;
+   private String lastName;
+   private String address;
+   public String getFirstName() {
+      return firstName;
+   }
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+   public String getLastName() {
+      return lastName;
+   }
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+   public String getAddress() {
+      return address;
+   }
+   public void setAddress(String address) {
+      this.address = address;
+   }
+}
